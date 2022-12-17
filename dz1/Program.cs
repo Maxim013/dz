939b509b77ad47948251b2ab019f517db6790a52
Maxim <maxim20030202@mail.ru>
2022-12-17 20:33:22 +0300

    dz1

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -4,11 +4,6 @@ int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число ");
 int b = int.Parse(Console.ReadLine()!);
 
-Console.Write("Введите второе число ");
-int c = int.Parse(Console.ReadLine()!);
-
-
-
 Console.WriteLine("Проверим является ли первое число квадратом второго");
 if (b*b==a){
     Console.WriteLine("Первое число является квадратом второго.");
