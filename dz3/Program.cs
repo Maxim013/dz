ad89ee79a85a3a2492b684185c9302bd062ff9df
Maxim <maxim20030202@mail.ru>
2022-12-17 20:15:50 +0300

    максимальное из 3 чисел

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int с = int.Parse(Console.ReadLine()!);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (с > max) max = с;
+Console.WriteLine("max = ");
+Console.WriteLine(max);
